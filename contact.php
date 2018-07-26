e7dbdcda1ee222dc4c1f6da69e1cc94bc6812109
Carolina Gonzalez <35787396+CarolinaGonzalezCA@users.noreply.github.com>
2018-07-26 13:57:56 -0500

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,62 @@
+<!Doctype html>
+
+<html>
+
+<head>
+
+<meta charset="UTF-8">
+
+<title>Carolina Gonzalez Contact Me</title> -->
+
+<link rel="stylesheet" href="css/main.css">
+
+</head>
+
+<body bgcolor="#98AFC7">
+
+<?php include 'header.php'?>
+
+<td>
+
+	<font color = "blue" size = "9">Thanks for Viewing!</font><font color="orange" size = "9">"Enter the following information:</font>
+	</td>
+	
+		   <div id="formDiv">
+
+          <h1>Contact Me</h1>																	
+
+          <form>
+
+           
+
+            Name<br> 
+
+            <input type="text" name="name" class="inputField" required><br>						
+
+            
+
+            Email Address<br>
+
+            <input type="email" name="email" class="inputField" required><br>					
+
+            
+
+            Message<br>	
+
+            <input type="textarea" name="message" class="textAreaInput" required><br>			
+
+            
+
+            <input type="submit" class="submitButton">											
+
+          </form>
+
+        </div>
+		
+		<?php include 'footer.php'?>
+		
+    </div>
+		  
+</body>
+
+</html>
