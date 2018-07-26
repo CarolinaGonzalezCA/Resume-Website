06a9333cbf88550fd496ac74804abd1d7e81c5c8
Carolina Gonzalez <35787396+CarolinaGonzalezCA@users.noreply.github.com>
2018-07-26 13:58:20 -0500

    Add files via upload

---
+++ education.php
@@ -0,0 +1,60 @@
+<!Doctype html>
+
+<html>
+
+<head>
+
+<meta charset="UTF-8">
+
+<title>Carolina Gonzalez Education</title>							
+
+<link rel="stylesheet" href="css/main.css">
+
+
+
+</head>
+
+
+<body bgcolor="6AFB92">
+
+<?php include 'header.php'?>
+
+
+  <div class="wrapper">
+
+
+    <div class="contentWrapper">
+
+
+        <div class="mainContent">
+       
+
+          <h1>Education</h1>        												
+
+
+          <h2>The University Of Southern Mississippi</h2>							
+
+          <p>Bachelors of Science in Computer Science<br>							
+
+          Long Beach, MS - <em>2017 - 2019</em></p>									
+
+      
+          <h2>Mississippi Gulf Coast Community College</h2>							
+
+          <p>Associates of Applied Science<br>										
+
+          Gulfport, MS - <em>2014 - 2017</em></p>									
+	
+	</div>
+	
+    <?php include 'footer.php'?>
+
+  </div>
+
+  
+
+</body>
+
+</html>
+
+          
