ec796a545247eb299c209bf40f71a5f2703f98ae
Carolina Gonzalez <35787396+CarolinaGonzalezCA@users.noreply.github.com>
2018-07-26 13:58:48 -0500

    Add files via upload

---
+++ experience.php
@@ -0,0 +1,94 @@
+<!Doctype html>
+
+<html>
+
+<head>
+
+<meta charset="UTF-8">
+
+						
+
+<link rel="stylesheet" href="css/main.css">
+
+
+
+</head>
+
+
+<body bgcolor="#3B9C9C">
+
+  <?php include"header.php" ?>
+
+
+  <div class="wrapper">
+
+
+    <div class="contentWrapper">
+
+
+        <div class="mainContent">
+       
+
+          <h1>Experience</h1>        														
+
+
+          <h2>Golden Nugget Casino</h2>														
+
+          <p>Retail Associate<br>															
+
+          Biloxi, MS - <em>2016 - 2018</em></p>												
+
+		  
+          <p>Greet customers and assist them with clothing selection. Maintain the store clean and organized.			
+   		  Perform returns and special transactions through the POS system. Trained new employees per corporation guidelines.
+		  Responsibilities included: cleaning, restocking merchandise, opening, and closing store.</p>
+
+          
+
+          <h2>Golden Nugget Casino</h2>														
+
+          <p>Box Office<br>															
+
+          Biloxi, MS - <em>2017 - 2018</em></p>												
+
+		  
+          <p>To answer telephone enquiries about the concerts.
+		  To operate the box office software, by using ticket master and the POS system comps to purchase tickets.</p>
+
+          
+
+          <h2>Burke Williams Day Spa</h2>													
+
+          <p>Spa Attendant<br>
+
+          Sherman Oaks, CA - <em>2013 - 2014</em></p>								
+		  
+          <p>Greet customers and direct them to the right treatment room.
+		  Manage sanitizing and cleaning as instructed.
+		  Maintain inventory of spa items and ensure constant supply</p>
+
+          
+
+          <h2>Burke Williams Corporate Office</h2>											
+
+          <p>File Organizer<br>																
+
+          Culver City, CA - <em>2013</em></p>												
+
+          <p>Review files, records, and other documents to obtain information to respond to requests.
+    		  Assist in filing duties</p>       
+          
+
+        </div>
+		
+
+  <?php include"footer.php" ?>
+
+
+  </div>
+
+  
+
+</body>
+
+</html>
