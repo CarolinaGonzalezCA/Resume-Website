e27013266e3c1db543e3ebae5fbf6335aa7581bd
Carolina Gonzalez <35787396+CarolinaGonzalezCA@users.noreply.github.com>
2018-07-26 13:59:38 -0500

    Add files via upload

---
+++ header.php
@@ -0,0 +1,26 @@
+<div id = "header">
+<DOCTYPE HTML>
+<html>
+<head>
+<title>Carolina- My resume webpage.</title>								
+<link href="style.css" rel="stylesheet" type="text/css">
+
+
+</head>
+<body bgcolor="#00FFFF">
+
+
+<table width = "800px" align "right" bgcolor= "white" border = "0" cellpadding="10">
+<tr>
+	<td>
+
+	<font color = "lime" size = "9">Hi!</font><font color="grey" size = "7">I'm Carolina Gonzalez</font>
+	</td>
+
+	<td>
+	<p><a href="contact.html"> Contact</a> | <a href= "experience.html">Experience</a> | <a href ="skills.html">Skills</a>| <a href="education.html">Education</a></p>
+	</td>
+<tr>
+</table>
+
+</body>
