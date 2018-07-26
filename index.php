98368ec2d96ed6010d85029fa43ddd60f6c10040
Carolina Gonzalez <35787396+CarolinaGonzalezCA@users.noreply.github.com>
2018-07-26 13:59:58 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<DOCTYPE HTML>
+<html>
+<head>
+<title>Carolina- My resume webpage.</title>								
+<link href="style.css" rel="stylesheet" type="text/css">
+
+
+</head>
+<body bgcolor="#00FFFF">
+
+<?php include"header.php"?>
+
+
+<img src="myself.jpg" width="400", height = "420"> 
+
+
+<h1 align ="justify">Welcome to my Resume Webpage</h1>     			
+										
+
+			<p>I'm a full-time student at the University of Southern Mississippi. I' am working on my degree in Computer Science
+		     with a minor in Biology and Chemistry.<br> I currently live in Saucier, MS. I'm originally from Moreno Valley, CA.
+			I' am single with no kids. I have 2 nieces that I consider my own. I don't have any pets.<br>
+			I enjoy writing code. I'm in my final year of school. I' am preparing to put my skills to use and find a career that I' am passionate about. <br>
+    
+    
+      My favorite quote is: <br> 
+			If you Fail, never give up because<br>
+			F.A.I.L means<br>
+			"First attempt in Learning"<br>
+			- End is not the end, in fact <br>
+			E.N.D means<br>
+			"Effort Never Dies"<br>
+			- If you get no as an answer,<br>
+			Remember N.O means<br>
+			"Next Opportunity".<br>
+			So lets be positive.<br>
+			- Dr. APJ Abdul Kalam</p> 
+		  
+								
+</body>
+</html>
+
+
+
+<p>Link to GitHub:</p>
+<p><a href="https://github.com/CarolinaGonzalezCA/Resume-Website">Account</a></p>
+
+<?php include"footer.php" ?>
+
+
+</body>
+</html>
