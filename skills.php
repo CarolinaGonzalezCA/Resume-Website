bd4275d9d6e95c218243f9b7372ef15c43bf8c91
Carolina Gonzalez <35787396+CarolinaGonzalezCA@users.noreply.github.com>
2018-07-26 14:00:27 -0500

    Add files via upload

---
+++ skills.php
@@ -0,0 +1,50 @@
+<!Doctype html>
+
+<html>
+
+<head>
+
+<meta charset="UTF-8">
+
+<title>Carolina Gonzalez Skills</title>							
+
+<link rel="stylesheet" href="css/main.css">
+
+</head>
+
+<body bgcolor="lavendar">
+
+<?php include 'header.php'?>
+
+
+          <h1>Skills</h1>        									
+		  
+<ul>																	
+  <li>Knows medical terminology</li>									
+  <li>Fluent in Spanish and English</li>		
+  <li>Proficient in:													
+    <ul>
+    <li>Microsoft Visual C++</li>
+    <li>Microsoft Visual Basic</li>
+    </ul>
+  </li>
+  <li>Knowledgeable upon:</li>											
+	<ul>
+  <li>using a spreadsheet</li>
+  <li>Word documents</li>
+  <li>a printer</li>
+	</ul>
+  </li>
+  <li>Familiar with:</li>												
+	<ul>
+  <li>Java</li>
+  <li>Python</li>
+	</ul>
+  </li>
+  
+  
+  <?php include"footer.php" ?>
+
+	
+</body>
+</html>
